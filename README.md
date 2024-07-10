07dadf3d41386ff03370bc7bfd0a68b6d675602e
rabiyatahseen <166829575+rabiyatahseen@users.noreply.github.com>
2024-07-10 20:29:11 +0530

    Update and rename README.md to helloworld.java

--- README.md
+++
@@ -1,2 +0,0 @@
-# JAVA
-programms for java
---
+++ helloworld.java
@@ -0,0 +1,7 @@
+# JAVA
+programms for java
+public class helloworld{
+    public static void main(String[]args){
+                System.out.println("hello world");
+    }
+}
