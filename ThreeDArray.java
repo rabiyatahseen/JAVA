a51762cbb335bcc46dc35fe7c27129d9be1d2a7b
Rabiya Tahseen <166829575+rabiyatahseen@users.noreply.github.com>
2024-08-28 11:51:18 +0530

    Add files via upload

---
+++ ThreeDArray.java
@@ -0,0 +1,17 @@
+public class ThreeDArray {
+    public static void main(String[] args) {
+        int [][][] arr = new int [2][2][2];
+        for (int i = 0; i < 2; i++) {
+            for (int j = 0; j < 2; j++) {
+            	for (int k = 0; k < 2; k++) {
+              		System.out.print(arr[i][j][k]);
+              		System.out.print(" ");
+            	}
+            	System.out.println();
+            }
+            System.out.println();
+        }
+ 
+    }
+}
+
